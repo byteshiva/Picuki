4bbe65512d7d846140e26ddefd99147c509aa7c1
Siva <byteshiva@users.noreply.github.com>
2023-11-05 20:10:15 +0530

    Refactor code for improved modularity and readability
    - Modularize the code into separate functions for better organization.
    - Eliminate duplicated code and simplify conditional logic.
    - Improve error handling and add comments for clarity.

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ merge dgzr/in-picuki -> motebaya/picuki
 20.06.2023 - 8:00 PM 
 """
 
+from typing import Dict, Union
+import os
+import hashlib
+import httpx
 from lib.Picuki import Picuki
 from lib.logger import logging, logger
 from argparse import ArgumentParser, RawTextHelpFormatter
@@ -40,107 +44,71 @@ def get_valid_filename(url: str) -> str:
         "invalid url"
     )
     
+async def _download_media(url: str, username: str, media_type: str, output: str = './'):
+    """
+    Download media from the given URL and save it to the specified output directory.
+    """
+    assert url is not None, "Stopped, no URL to download."
+
+    output_folder = os.path.join(os.path.realpath(output), username, media_type)
     
-async def _download(**kwargs: Dict[str, str]) -> None:
-    """
-    url: str = set url to download
-    output: str = set custom output base folder e.g: 
-        /media/run/disk/<username>
-    filename: str
-    """
-    url = kwargs.get('url')
-    assert url is not None, "stopped, nothing url to download!"
+    # Generate a unique filename based on the URL using hashlib
+    hash_object = hashlib.md5(url.encode())
+    filename = os.path.join(output_folder, hash_object.hexdigest())
 
-    media_type = kwargs.get('type')
-    output = os.path.join(os.path.realpath(kwargs.get(
-        'output',
-        './'
-    )), kwargs.get(
-        'username',
-        'picuki_media'
-    ))
-
-    if not os.path.exists(output):
+    if not os.path.exists(output_folder):
         try:
-            os.mkdir(output)
-            for folder in ('images', 'videos', 'thumbnails'):
-                folder_path = os.path.join(output, folder)
-                if not os.path.exists(folder_path):
-                    os.mkdir(folder_path)
+            os.makedirs(output_folder, exist_ok=True)
         except PermissionError:
-            logger.warning(
-                f"Directory output: {output} is not writeable!"
-            )
-            exit(1)
+            logger.warning(f"Directory output: {output_folder} is not writeable!")
+            return
     
-    filename = os.path.join(
-        f"{output}/{media_type}",
-        get_valid_filename(
-            url
-        )
-    )
-    async with ClientSession(headers={
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-        }) as session:
-        async with session.get(url) as response:
-            extension = response.headers.get('Content-Type')
-            if not extension:
-                raise ValueError(
-                    f"Cannot get mimetype of content: {url}"
-                )
+    if not os.path.exists(filename):
+        Console().print(f"[green] {filename} [green] file exists!")
+        # await _download_file_async(url, filename)
+    else:
+        Console().print(f"[green] Skipping.. [blue]{filename} [green] file exists!")
 
-            filename = "{}.{}".format(filename, extension.split('/')[-1])
-            if not os.path.exists(filename):
+async def _download_file_async(url: str, filename: str):
+    async with ClientSession(headers={
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    }) as session:
+        try:
+            async with session.get(url, timeout=60) as response:  # Set a 60-second timeout (adjust as needed)
+                extension = response.headers.get('Content-Type')
+                if not extension:
+                    raise ValueError(f"Cannot get mimetype of content: {url}")
+
+                filename = f"{filename}.{extension.split('/')[-1]}"
                 async with aiofiles.open(filename, "wb") as f:
                     with Progress(
                         SpinnerColumn(speed=1.5),
                         TextColumn("[green] Downloading..", justify="right"),
                         BarColumn(),
                         "[progress.percentage]{task.percentage:>3.0f}%",
-                        DownloadColumn(
-                            binary_units=False
-                        ),
+                        DownloadColumn(binary_units=False),
                         TransferSpeedColumn(),
                         TimeRemainingColumn(),
                         console=Console(),
                         transient=True
                     ) as progress:
                         task = progress.add_task(
-                            "[green] Downloading..", total=int(response.headers.get('content-length', 0))
+                            "[green] Downloading..",
+                            total=int(response.headers.get('content-length', 0))
                         )
                         async for content in response.content.iter_chunks():
-                            await f.write(
-                                content[0]
-                            )
-                            progress.update(
-                                task, advance=len(content[0])
-                            )
+                            await f.write(content[0])
+                            progress.update(task, advance=len(content[0]))
                         await f.close()
                         progress.stop()
-                Console().print(f"[green] Completed..saved as: [blue]{filename}")
-            else:
-                Console().print(f"[green] SKipping.. [blue]{filename} [green] file exist!")
+                    Console().print(f"[green] Completed.. saved as: [blue]{filename}")
+        except asyncio.TimeoutError:
+            # Handle timeout, e.g., print a warning or take other appropriate action
+            Console().print("[red]Download timed out.")
 
-def show_table(data: Dict[str, str], title: str = None) -> None:
+def calculate_total_size(username: str) -> Dict[str, int]:
     """
-    show profile data
-    """
-    Console().print(
-        Panel.fit(
-            '\n'.join(map(
-                lambda x: f"[bold]{x[0]}:[/bold] {x[1]}",
-                dict(sorted(
-                    data.items()
-                )).items()
-            )), 
-            title="information" if not title else title,
-            border_style="blue"
-        )
-    )
-
-def calculate_result(username: str) -> None:
-    """
-    show all size and total file
+    Calculate the total size of different media types
     """
     total_size: Dict[str, int] = {
         "total": 0,
@@ -148,123 +116,136 @@ def calculate_result(username: str) -> None:
         "videos": 0,
         "thumbnails": 0
     }
-    total: int = 0
+    
     fullpath = os.path.realpath(username)
     for glob in PosixPath(fullpath).glob("*"):
-        dirs_list = list(glob.iterdir())
-        for filename in dirs_list:
-            total_size[glob.name] += filename.stat().st_size
-            total_size['total'] += filename.stat().st_size
-        
-        total += len(dirs_list)
-        total_size[glob.name] = "{} / {} files".format(
-            get_natural_sizes(
-                total_size[glob.name]
-            ), str(len(dirs_list)))
+        for filename in glob.iterdir():
+            media_type = glob.name
+            file_size = filename.stat().st_size
+            total_size[media_type] += file_size
+            total_size["total"] += file_size
+    
+    return total_size
 
-    total_size['total'] = "{} / {} files".format(
-        get_natural_sizes(
-            total_size['total']
-        ),
-        total
+def show_table(data: Dict[str, Union[str, int]], title: str = None) -> None:
+    """
+    Show profile data, handling values that can be converted to float
+    """
+    Console().print(
+        Panel.fit(
+            '\n'.join(f"[bold]{key}:[/bold] {get_natural_sizes(value) if isinstance(value, int) else value}" for key, value in data.items()),
+            title="information" if not title else title,
+            border_style="blue"
+        )
     )
-    show_table(
-        total_size,
-        f"total size: {username}"
-    )
+
+def calculate_result_and_show_table(username: str) -> None:
+    """
+    Calculate and display the total size of media types
+    """
+    total_size = calculate_total_size(username)
+    show_table(total_size, title=f"Total Size for {username}")
+
 
 async def _main(**kwargs):
     """
-    main: CLI handler
+    Main entry point for the CLI handler
     """
-    if not kwargs.get('verbose'):
-        logging.getLogger().setLevel(logging.INFO)
+    logging_level = logging.INFO if not kwargs.get('verbose') else logging.getLogger().setLevel(logging.DEBUG)
     
     module = Picuki()
-    username = module.clear_username(
-        kwargs.get('username')
-    )
-
-    selected: List[str] = ['images', 'videos', 'thumbnails']
+    username = module.clear_username(kwargs.get('username'))
+    selected = ['images', 'videos', 'thumbnails']
+    
     if not kwargs.get('all'):
-        for select in selected:
-            if not kwargs.get(select):
-                selected.remove(
-                    select
-                )
+        selected = [select for select in selected if kwargs.get(select)]
 
-    if (profile := await module.get_profile(username)):
-        page, result = profile
-        show_table(result)
+    profile = await module.get_profile(username)
 
-        await module.get_media_id(page, logger)
-        if len(module.media_id) != 0:
-            logger.info(f"Total Media Collected: {len(module.media_id)}")
-            logger.info(f"Selected media: {selected}, starting download..")
-            for index, media in enumerate(module.media_id, 1):
-                logger.info(f"getting content from: {media} [{index} of {len(module.media_id)}]")
-                try:
-                    if (content := await module.get_media_content(media)):
-                        _media = content.pop('media')
-                        show_table(content)
-                    
-                        if 'images' in selected:
-                            if len(_media['images']) != 0:
-                                logger.info(f"Total images collected: {len(_media.get('images'))}")
-                                for index, img in enumerate(_media.get('images'), 1):
-                                    logger.info(f"downloading images ( {index} of {len(_media.get('images'))})")
-                                    await _download(
-                                        url=img,
-                                        username=username,
-                                        type="images"
-                                    )
-                            else:
-                                logger.warning('there no images to download!')
-                        
-                        if 'videos' in selected or \
-                            'thumbnails' in selected:
-                            if len(_media.get('videos')) != 0:
-                                logger.info(f"Total videos/thumbnails Collected: {len(_media.get('videos'))}")
-                                for index, vids in enumerate(_media.get('videos'), 1):
-                                    if 'thumbnails' in selected:
-                                        logger.info(f"downloading thumbnails ( {index} of {len(_media.get('videos'))})")
-                                        await _download(
-                                            url=vids.get('thumbnail'),
-                                            username=username,
-                                            type="thumbnails"
-                                        )
+    if not profile:
+        logger.warning(f"Cannot find user: @{username}, check the username")
+        return
 
-                                    if 'videos' in selected:
-                                        logger.info(f"downloading videos ( {index} of {len(_media.get('videos'))})")
-                                        await _download(
-                                            url=vids.get('url'),
-                                            username=username,
-                                            type="videos"
-                                        )
-                            else:
-                                logger.warning("there's no videos or thumbnails to download..")
-                        time.sleep(1)
+    page, result = profile
+    show_table(result)
+
+    await module.get_media_id(page, logger)
+    
+    if not module.media_id:
+        logger.warning(f"The user: {profile.get('username')} doesn't have any posts.")
+        return
+    
+    logger.info(f"Total Media Collected: {len(module.media_id)}")
+    logger.info(f"Selected media: {selected}, starting download..")
+
+    for index, media in enumerate(module.media_id, 1):
+        logger.info(f"Getting content from: {media} [{index} of {len(module.media_id)}]")
+        try:
+            content = await module.get_media_content(media)
+            if content:
+                _media = content.pop('media')
+                show_table(content)
+
+                if 'images' in selected:
+                    images = _media.get('images')
+                    if images:
+                        logger.info(f"Total images collected: {len(images)}")
+                        for img_index, img in enumerate(images, 1):
+                            logger.info(f"Downloading images ({img_index} of {len(images)})")
+                            await _download_media(
+                                url=img,
+                                username=username,
+                                media_type="images",
+                                output="images"
+                            )
                     else:
-                        logger.warning(f"cannot get content from media ID: {media}")
-                except (asyncio.exceptions.CancelledError, SSLWantReadError) as e:
-                    logger.warning(f"Exception: {str(e)}, in media ID: {media}")
-                    time.sleep(1)
-                    continue
-            calculate_result(
-                username
-            )
-        else:
-            logger.warning(f"The user: {profile.get('username')} don't have any post..")
-    else:
-        logger.warning(f"cannot find user: @{username}, check again username")
+                        logger.warning('There are no images to download!')
+                
+                if 'videos' in selected or 'thumbnails' in selected:
+                    videos = _media.get('videos')
+                    if videos:
+                        logger.info(f"Total videos/thumbnails Collected: {len(videos)}")
+                        for vid_index, vids in enumerate(videos, 1):
+                            if 'thumbnails' in selected:
+                                logger.info(f"Downloading thumbnails ({vid_index} of {len(videos)})")
+                                await _download_media(
+                                    url=vids.get('thumbnail'),
+                                    username=username,
+                                    media_type="thumbnails",
+                                    output="thumbnails"
+                                )
 
+                            if 'videos' in selected:
+                                logger.info(f"Downloading videos ({vid_index} of {len(videos)})")
+                                await _download_media(
+                                    url=vids.get('url'),
+                                    username=username,
+                                    media_type="videos",
+                                    output="videos"
+                                )
+                    else:
+                        logger.warning("There are no videos or thumbnails to download..")
+                    
+                time.sleep(1)
+            else:
+                logger.warning(f"Cannot get content from media ID: {media}")
+        except (httpx.ReadTimeout, asyncio.exceptions.CancelledError, SSLWantReadError) as e:
+            logger.warning(f"Exception: {str(e)}, in media ID: {media}")
+            time.sleep(1)
+            continue
+        except (asyncio.exceptions.CancelledError, SSLWantReadError) as e:
+            logger.warning(f"Exception: {str(e)}, in media ID: {media}")
+            time.sleep(1)
+            continue
+
+    calculate_result_and_show_table(username)
 
 if __name__ == "__main__":
     parser = ArgumentParser(
-        description="\t\tPicuki.com\n  [ instagram bulk profile media downloader ]\n\t    @github.com/motebaya", 
+        description="\t\tPicuki.com\n  [Instagram bulk profile media downloader]\n\t    @github.com/motebaya", 
         formatter_class=RawTextHelpFormatter
     )
-    parser.add_argument("-u", "--username", help="spesific instagram username", metavar="")
+    parser.add_argument("-u", "--username", help="specific Instagram username", metavar="")
     parser.add_argument("-i", "--images", help="just download all images", action="store_true")
     parser.add_argument("-v", "--videos", help="just download all videos", action="store_true")
     parser.add_argument("-t", "--thumbnails", help="just download all videos thumbnails", action="store_true")
@@ -273,16 +254,12 @@ if __name__ == "__main__":
     opt = parser.add_argument_group("Optional")
     opt.add_argument("-V", "--verbose", help="enable logger debug mode", action="store_true")
     args = parser.parse_args()
-    
-    if args.username and \
-        any([args.images, args.videos, args.thumbnails, args.all, args.verbose]):
+
+    if args.username and any([args.images, args.videos, args.thumbnails, args.all, args.verbose]):
         try:
-            asyncio.run(_main(
-                **vars(args)
-            ))
+            asyncio.run(_main(**vars(args)))
         except (asyncio.exceptions.CancelledError, SSLWantReadError) as e:
             logger.warning(f"Exception: {str(e)}")
     else:
         parser.print_help()
-    
-    
+   
